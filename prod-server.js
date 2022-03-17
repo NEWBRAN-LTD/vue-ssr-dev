e751a337c217b9512b67f3ec4c6f62d884706b11
Joel Chu <joelckchu@gmail.com>
2022-03-18 06:40:16 +0800

    implement our own serve static

--- prod-server.js
+++ prod-server.js
@@ -1,17 +1,56 @@
-// This is the production server using uWebSocket.js
-
+// @ts-check
+const fs = require('fs')
+const path = require('path')
 const uWS = require('uWebSockets.js')
+const mime = require('mime-types')
+
 const port = process.env.PORT || 9001
+// This is the production server using uWebSocket.js
+const resolve = (p) => path.resolve(__dirname, p)
 
 uWS.
   App({})
-  .get('/*', (res, req) => {
+  // serve up the static files
+  .get('/assets/*', async (res, req) => {
+    const url = req.getUrl()
+    const assetDir = path.join(__dirname, 'dist', 'client')
+    const file = fs.readFileSync(path.join(assetDir, url))
+    if (file) {
+      const mimeType = mime.lookup(url) || 'application/octet-stream'
+      console.log(mimeType, url)
+      res.writeHeader('Content-Type', mimeType)
+      res.end(file)
+    } else {
+      // @TODO
+      res.writeStatus(404)
+    }
+
+  })
+  .get('/*', async (res, req) => {
 
     // Here we modified it for our SSR app
+    const indexProd = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+    const manifest = /* @ts-ignore */ require('./dist/client/ssr-manifest.json')
+    const url = req.getUrl()
 
-    console.log(res)
+    // now we just need to figure out how to serve the static files
+    // and we really shouldn't serve static files here, it should be
+    // the job of web server
 
-    res.end('Hello World!');
+    console.log(`request url`, url)
+    // skip the serve static for now
+    let template, render
+    template = indexProd
+    render = require('./dist/server/entry-server.js').render
+
+
+    const [appHtml, preloadLinks] = await render(url, manifest)
+
+    const html = template
+      .replace(`<!--preload-links-->`, preloadLinks)
+      .replace(`<!--app-html-->`, appHtml)
+
+    res.end(html)
 
 
   }).listen(port, (token) => {
